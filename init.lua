7687df4a7a47f2efe003b1e7e2be749bbe3b20c3
Hind Sagar Biswas <hindsbhk@gmail.com>
2024-10-25 20:20:50 +0600

    incline, refactoring

--- init.lua
+++ init.lua
@@ -167,6 +167,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Lazygit
 vim.keymap.set('n', '<A-q>', ':LazyGit<CR>')
 
+-- Select All
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
---
+++ lua/custom/plugins/refactoring.lua
@@ -0,0 +1,18 @@
+return {
+  'ThePrimeagen/refactoring.nvim',
+  keys = {
+    {
+      '<leader>rf',
+      function()
+        require('refactoring').select_refactor {
+          show_success_message = true,
+        }
+      end,
+      mode = 'v',
+      noremap = true,
+      silent = true,
+      expr = false,
+    },
+  },
+  opts = {},
+}
